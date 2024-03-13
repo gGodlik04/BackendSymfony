9dc38db126f732d4d7f47a6001dd4e99e0505cdb
Eduard_Galikhanov <Galikhanov@infomatika.ru>
2024-03-13 17:59:30 +0300

    chore: Add phpunit write test

---
+++ tests/HealthCheckAсtionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class HealthCheckAсtionTestTest extends WebTestCase
+{
+    public function test_request_responded_successful_result(): void
+    {
+        $client = static::createClient();
+        $client->request(Request::METHOD_GET, '/health-check');
+
+        $this->assertResponseIsSuccessful();
+        $jsonResponse = json_encode($client->getResponse()->getContent(), true);
+        $this->assertEquals($jsonResponse['status'], 'ok');
+    }
+}
